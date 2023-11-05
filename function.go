7f969ebfa0c0ed1cca7833bad449f7b2e780bba8
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-05 11:03:25 +0700

    update dependensi

--- function.go
+++ function.go
@@ -7,6 +7,8 @@ import (
 	"os"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/aiteung/atapi"
+	"github.com/aiteung/atmessage"
 	"github.com/aiteung/module/model"
 )
 
@@ -22,9 +24,22 @@ func WebHookPost(w http.ResponseWriter, r *http.Request) {
 		resp = "error parsing application/json: " + err.Error()
 	} else if r.Header.Get("Secret") == os.Getenv("SECRET") {
 		resp = "berhasil"
+		dt := &TextMessage{
+			To:       "6285155476774",
+			IsGroup:  false,
+			Messages: "123",
+		}
+		response := atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://wa.my.id/send/message/text")
+		resp = response.Response
 	} else {
 		resp = "Secret Salah"
 	}
 	fmt.Fprintf(w, resp)
 
+}
+
+type TextMessage struct {
+	To       string `json:"to"`
+	IsGroup  bool   `json:"isgroup,omitempty"`
+	Messages string `json:"messages"`
 }
