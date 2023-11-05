1d326f38d36d9fc27ce95e5524b00c59ab620856
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-05 14:08:21 +0700

    whatsauth

--- function.go
+++ function.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/aiteung/atapi"
@@ -19,17 +20,28 @@ func WebHookPost(w http.ResponseWriter, r *http.Request) {
 	var resp Response
 	json.NewDecoder(r.Body).Decode(&msg)
 	if r.Header.Get("Secret") == os.Getenv("SECRET") {
-		dt := &TextMessage{
-			To:       msg.Phone_number,
-			IsGroup:  false,
-			Messages: "Hai hai hai kak " + msg.Alias_name,
+		if strings.Contains(msg.Message, WAKeyword) && msg.From_link { //untuk whatsauth request login
+			dt := &WhatsauthRequest{
+				Uuid:        strings.Replace(msg.Message, WAKeyword, "", 1),
+				Phonenumber: msg.Phone_number,
+				Delay:       msg.From_link_delay,
+			}
+			resp, _ = atapi.PostStructWithToken[Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/whatsauth/request")
+		} else { //untuk membalas pesan masuk
+			dt := &TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: "Hai hai hai kak " + msg.Alias_name,
+			}
+			resp, _ = atapi.PostStructWithToken[Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
 		}
-		resp, _ = atapi.PostStructWithToken[Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
 	} else {
 		resp.Response = "Secret Salah"
 	}
 	fmt.Fprintf(w, resp.Response)
 }
+
+var WAKeyword = "wh4t5auth0"
 
 type Response struct {
 	Response string `json:"response"`
@@ -39,6 +51,12 @@ type TextMessage struct {
 	To       string `json:"to"`
 	IsGroup  bool   `json:"isgroup,omitempty"`
 	Messages string `json:"messages"`
+}
+
+type WhatsauthRequest struct {
+	Uuid        string `json:"uuid,omitempty" bson:"uuid,omitempty"`
+	Phonenumber string `json:"phonenumber,omitempty" bson:"phonenumber,omitempty"`
+	Delay       uint32 `json:"delay,omitempty" bson:"delay,omitempty"`
 }
 
 type WAMessage struct {
