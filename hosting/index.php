bee86ac577e2ed854aba553fe9b9d3f941cd31bf
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-06 15:11:21 +0700

    php

--- hosting/index.php
+++ hosting/index.php
@@ -39,4 +39,53 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     http_response_code(405);
     echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
 }
+
+
+// The JSON data.
+$data = array(
+    'to' => '6281111',
+    'isgroup' => false,
+    'messages' => 'hai kak'
+);
+
+// JSON encode the data.
+$jsonData = json_encode($data);
+
+// The URL to send the POST request to.
+$url = 'http://example.com/endpoint'; // Replace with the URL of the server you're sending the request to.
+
+// The token to be sent in the header.
+$token = 'YourTokenHere'; // Replace this with your actual token.
+
+// Initialize cURL session.
+$ch = curl_init($url);
+
+// Set cURL options.
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Request-Token: ' . $token // Add your token header here
+));
+
+// Execute the POST request.
+$response = curl_exec($ch);
+
+// Check for cURL errors
+if (curl_errno($ch)) {
+    $error_msg = curl_error($ch);
+}
+
+// Close cURL session.
+curl_close($ch);
+
+// If there was an error, handle it here.
+if (isset($error_msg)) {
+    // Log or echo the error message.
+    echo "cURL Error: " . $error_msg;
+}
+
+// Do something with the response.
+echo $response;
 ?>
