fe15b7e6d9db3d2bc9b477a03f763e93d5e28407
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-06 19:56:16 +0700

    sip

--- hosting/index.php
+++ hosting/index.php
@@ -1,12 +1,16 @@
 <?php
 // Define your secret token here
-define('SECRET_TOKEN', 'your_secret_token');
+define('SECRET_TOKEN', 'secrettokenkakak');
+define('TOKEN', 'v4.public.saoid98j98uj89m8j');
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Get the token from the request headers
     $headers = apache_request_headers();
-    $requestToken = isset($headers['Request-Token']) ? $headers['Request-Token'] : '';
+    $requestToken = isset($headers['secret']) ? $headers['secret'] : '';
+    if ($requestToken==''){
+        $requestToken = isset($headers['Secret']) ? $headers['Secret'] : '';
+    }
 
     // Verify the token
     if ($requestToken === SECRET_TOKEN) {
@@ -18,12 +22,48 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         // Check if JSON decoding was successful
         if ($data) {
-            // Use $data as an associative array with your WAMessage struct fields
-            // For example, access phone_number like $data['phone_number']
-            // Here, you would process the data or save it to a database, etc.
-
-            // Send a response back
-            echo json_encode(['status' => 'success', 'message' => 'Data received and processed']);
+            // The JSON data.
+            $msg = array(
+                'to' => $data["phone_number"],
+                'isgroup' => false,
+                'messages' => 'hai kak ',
+            );
+            // JSON encode the data.
+            $jsonData = json_encode($msg);
+            // The URL to send the POST request to.
+            //$url = 'https://api.wa.my.id/api/send/message/text';
+            $url = 'https://eov6tgpfbhsve67.m.pipedream.net';
+            // Initialize cURL session.
+            $ch = curl_init($url);
+            // Set cURL options.
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+                'accept: application/json',
+                'Content-Type: application/json',
+                'token: ' . TOKEN // Add your token header here
+            ));
+            
+            // Execute the POST request.
+            $response = curl_exec($ch);
+            
+            // Check for cURL errors
+            if (curl_errno($ch)) {
+                $error_msg = curl_error($ch);
+            }
+            
+            // Close cURL session.
+            curl_close($ch);
+            // If there was an error, handle it here.
+            if (isset($error_msg)) {
+                // Log or echo the error message.
+                echo "cURL Error: " . $error_msg;
+            }
+            
+            // Do something with the response.
+            echo $response;
+            //echo json_encode($msg);
         } else {
             // JSON was not decoded, handle the error
             http_response_code(400);
@@ -39,53 +79,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     http_response_code(405);
     echo json_encode(['status' => 'error', 'message' => 'Method not allowed']);
 }
-
-
-// The JSON data.
-$data = array(
-    'to' => '6281111',
-    'isgroup' => false,
-    'messages' => 'hai kak'
-);
-
-// JSON encode the data.
-$jsonData = json_encode($data);
-
-// The URL to send the POST request to.
-$url = 'http://example.com/endpoint'; // Replace with the URL of the server you're sending the request to.
-
-// The token to be sent in the header.
-$token = 'YourTokenHere'; // Replace this with your actual token.
-
-// Initialize cURL session.
-$ch = curl_init($url);
-
-// Set cURL options.
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-    'Content-Type: application/json',
-    'Request-Token: ' . $token // Add your token header here
-));
-
-// Execute the POST request.
-$response = curl_exec($ch);
-
-// Check for cURL errors
-if (curl_errno($ch)) {
-    $error_msg = curl_error($ch);
-}
-
-// Close cURL session.
-curl_close($ch);
-
-// If there was an error, handle it here.
-if (isset($error_msg)) {
-    // Log or echo the error message.
-    echo "cURL Error: " . $error_msg;
-}
-
-// Do something with the response.
-echo $response;
 ?>
