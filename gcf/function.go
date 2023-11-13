7b556766933bd087cdd5114cd5ad4c8db0c512bb
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-14 06:50:05 +0700

    oke sip coy

--- gcf/function.go
+++ gcf/function.go
@@ -2,9 +2,11 @@ package gcf
 
 import (
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/gofiber/fiber/v2/middleware/adaptor"
 	"github.com/whatsauth/webhook"
 )
 
 func init() {
-	functions.HTTP("WebHook", webhook.Post)
+	//functions.HTTP("WebHook", webhook.Post)
+	functions.HTTP("WebHook", adaptor.FiberHandlerFunc(webhook.PostMessage))
 }
