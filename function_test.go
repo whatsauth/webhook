c8b8f936eecf3e214adad9d026b7d560b1448afa
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-13 19:35:55 +0700

    Post:handling group message

--- function_test.go
+++ function_test.go
@@ -3,20 +3,17 @@ package webhook
 import (
 	"fmt"
 	"os"
-	"strings"
 	"testing"
-
-	"github.com/aiteung/atdb"
 )
 
 func TestGetENVToken(t *testing.T) {
 	fmt.Println(os.Getenv("MONGOSTRINGAWANGGA"))
 }
 
-func TestUpdateGetData(t *testing.T) {
+/* func TestUpdateGetData(t *testing.T) {
 	result, err := atdb.GetRandomDoc[Reply](Mongoconn, "reply", 1)
 	if err != nil {
 		t.Error(err)
 	}
 	fmt.Printf("%+v\n", strings.ReplaceAll(result[0].Message, "#BOTNAME#", "aku"))
-}
+} */
--- handler.go
+++ handler.go
@@ -3,13 +3,14 @@ package webhook
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/aiteung/atapi"
 	"github.com/aiteung/atmessage"
 	"github.com/aiteung/module/model"
 	"github.com/whatsauth/wa"
 	"github.com/whatsauth/ws"
-	"net/http"
-	"os"
 )
 
 func Post(w http.ResponseWriter, r *http.Request) {
@@ -27,9 +28,12 @@ func Post(w http.ResponseWriter, r *http.Request) {
 			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/whatsauth/request")
 		} else { //untuk membalas pesan masuk
 			dt := &wa.TextMessage{
-				To:       msg.Phone_number,
+				To:       msg.Chat_number,
 				IsGroup:  false,
 				Messages: "Hai hai hai kak " + msg.Alias_name,
+			}
+			if msg.Chat_server == "g.us" { //jika pesan datang dari group maka balas ke group
+				dt.IsGroup = true
 			}
 			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
 		}
