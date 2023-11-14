c411d3707d833e7ca038a6730a77377571c75d1f
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-14 07:08:19 +0700

    hapus kode yang tidak perlu

--- handler.go
+++ handler.go
@@ -7,23 +7,6 @@ import (
 	"github.com/whatsauth/ws"
 )
 
-/* func Post(w http.ResponseWriter, r *http.Request) {
-	var WAKeyword = "wh4t5auth0"
-	var msg model.IteungMessage
-	var resp atmessage.Response
-	json.NewDecoder(r.Body).Decode(&msg)
-	if r.Header.Get("Secret") == WebhookSecret {
-		if ws.IsLoginRequest(msg, WAKeyword) { //untuk whatsauth request login
-			resp = HandlerQRLogin(msg, WAKeyword)
-		} else { //untuk membalas pesan masuk
-			resp = HandlerIncomingMessage(msg)
-		}
-	} else {
-		resp.Response = "Secret Salah"
-	}
-	fmt.Fprintf(w, resp.Response)
-} */
-
 func PostMessage(c *fiber.Ctx) error {
 	var h Header
 	err := c.ReqHeaderParser(&h)
